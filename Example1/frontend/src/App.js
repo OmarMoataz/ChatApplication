d58b4ff8dd162de957eac6a315882c077e23f18a
Omar Moataz <omar.attia@itworx.com>
2019-01-16 17:57:32 +0200

    Created Message compose component and created event handler to pass message data to parent component. Adding that to the state array is not yet implemented.

--- Example1/frontend/src/App.js
+++ Example1/frontend/src/App.js
@@ -1,49 +1,66 @@
-import React, { Component } from 'react';
-import socketIOClient from 'socket.io-client';
-import { Message } from './Components/message/message';
-import { User } from './Components/user/user';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+import { Message } from "./Components/message/message";
+import { User } from "./Components/user/user";
+import ComposeMessage from "./Components/ComposeMessage/ComposeMessage";
 
 class App extends Component {
   constructor() {
     super();
-    
+
     this.state = {
-      endpoint: "http://localhost:3030" 
-    }
+      endpoint: "http://localhost:3030",
+      messages: []
+    };
   }
-  
+
   send = () => {
     const socket = socketIOClient(this.state.endpoint);
 
-    
-    socket.emit('test', 'test');
+    socket.emit("test", "test");
+  };
+
+  handleSubmit(message) {
+    console.log(this.messages);
+    // this.setState({
+    //   messages: [...this.state.messages, message]
+    // })
+    const messages = this.state.messages.slice();
+    messages.push(message);
+    this.setState({messages});
   }
-  
+
   render() {
     const socket = socketIOClient(this.state.endpoint);
-    
-    socket.on('msg', (msg) => {
+
+    socket.on("msg", msg => {
       console.log(`received msg ${msg}`);
-    })
-     
+    });
+
     return (
       <div>
-      <div className={"chat"}>
-      <User img="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" location="Cairo" name="Omar Moataz"/>
-      <div className={"messages"}>
-        <div className={"messages-content"}>
-          <Message author="a" msg="test" img="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" time="now" />
+        <div className={"chat"}>
+          <User
+            img="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg"
+            location="Cairo"
+            name="Omar Moataz"
+          />
+          <div className={"messages"}>
+            <div className={"messages-content"}>
+              <Message
+                author="a"
+                msg="test"
+                img="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg"
+                time="now"
+              />
+            </div>
+          </div>
+          <ComposeMessage onSubmit={this.handleSubmit} />
         </div>
+        <div className={"bg"} />
       </div>
-      <div className={"message-box"}>
-        <textarea type="text" className={"message-input"} placeholder="Type message..."></textarea>
-        <button type="submit" className={"message-submit"}>Send</button>
-      </div>
-  </div>
-  <div className={"bg"}></div>
-  </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ Example1/frontend/src/Components/ComposeMessage/ComposeMessage.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class ComposeMessage extends Component {
+  constructor(props) {
+      super(props);
+      this.messageReference = React.createRef();
+  }
+
+  updateInputValue() {
+    this.props.onSubmit(this.messageReference.current.value);
+  }
+  
+  render() {
+    return (
+      <div className={"message-box"}>
+        <textarea
+          ref={this.messageReference}
+          type="text"
+          className={"message-input"}
+          placeholder="Type message..."
+        />
+        <button onClick={() => {this.updateInputValue()}} type="submit" className={"message-submit"}>
+          Send
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ComposeMessage;
