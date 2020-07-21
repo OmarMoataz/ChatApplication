b1dacb8040b8371e1aef2797149a1392fc218edf
Omar <OmarMoataz@outlook.com>
2020-07-21 02:17:38 +0200

    Fixed issue with room image.

--- frontend/src/Components/user/user.js
+++ frontend/src/Components/user/user.js
@@ -6,7 +6,7 @@ export const User = (props) => {
       <h1> {props.name}</h1>
       <h2> {props.location} </h2>
       <figure className="avatar">
-        {props.image && <img src={props.image} alt="Profile" />}
+        {props.img && <img src={props.img} alt="Profile" />}
       </figure>
     </div>
   );
